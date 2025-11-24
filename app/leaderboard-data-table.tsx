f02abc753216f27fb3259e4ea4e2e0329ab69152
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2025-11-24 01:06:45 +0100

    feat: make head font bold

--- app/leaderboard-data-table.tsx
+++ app/leaderboard-data-table.tsx
@@ -39,7 +39,7 @@ export function LeaderboardTable<TData, TValue>({
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
                 return (
-                  <TableHead key={header.id}>
+                  <TableHead className={"font-bold"} key={header.id}>
                     {header.isPlaceholder
                       ? null
                       : flexRender(
