ec696316d40b38bbb20b18f31de47121b3c84c5d
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2025-11-26 22:34:46 +0100

    chore: format code

--- app/test/Chart.tsx
+++ app/test/Chart.tsx
@@ -1,71 +1,78 @@
-"use client"
+"use client";
 
-import { TrendingUp } from "lucide-react"
-import { Bar, BarChart, XAxis, YAxis } from "recharts"
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
 
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
-    ChartConfig,
-    ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
-} from "@/components/ui/chart"
-import {LeaderboardMember} from "@/src/types/leaderboard";
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { LeaderboardMember } from "@/src/types/leaderboard";
 
 const chartConfig = {
-    local_score: {
-        label: "Score",
-        color: "#69d804",
-    },
-} satisfies ChartConfig
+  local_score: {
+    label: "Score",
+    color: "#69d804",
+  },
+} satisfies ChartConfig;
 
 interface Props {
-    members: LeaderboardMember[];
-    lastUpdated: Date;
+  members: LeaderboardMember[];
+  lastUpdated: Date;
 }
 
 export default function Chart({ members, lastUpdated }: Props) {
-    return (
-        <div className={"flex h-dvh justify-center items-center"}>
-            <Card className={""}>
-                <CardHeader>
-                    <CardTitle>Total Score</CardTitle>
-                    <CardDescription>Last updated: {lastUpdated.toLocaleTimeString("de-DE") + " " + lastUpdated.toLocaleDateString("de-DE")}</CardDescription>
-                </CardHeader>
-                <CardContent className="w-5xl">
-                    <ChartContainer config={chartConfig}>
-                        <BarChart
-                            accessibilityLayer
-                            data={members}
-                            layout="horizontal"
-                            margin={{
-                                left: 0,
-                            }}
-                        >
-                            <YAxis type="number" dataKey="local_score" hide />
-                            <XAxis
-                                dataKey="name"
-                                type="category"
-                                tickLine={false}
-                                tickMargin={0}
-                                axisLine={false}
-                                tickFormatter={(value) => value.slice(0, 10) + "..."}
-                            />
-                            <ChartTooltip
-                                cursor={false}
-                                content={<ChartTooltipContent hideLabel />}
-                            />
-                            <Bar dataKey="local_score" fill="var(--accent)" radius={5} />
-                        </BarChart>
-                    </ChartContainer>
-                </CardContent>
-            </Card>
-        </div>
-    )
+  return (
+    <div className={"flex h-dvh justify-center items-center"}>
+      <Card className={"w-200"}>
+        <CardHeader>
+          <CardTitle>Total Score</CardTitle>
+          <CardDescription>
+            Last updated:{" "}
+            {lastUpdated.toLocaleTimeString("de-DE") +
+              " " +
+              lastUpdated.toLocaleDateString("de-DE")}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="">
+          <ChartContainer config={chartConfig}>
+            <BarChart
+              accessibilityLayer
+              data={members}
+              layout="horizontal"
+              margin={{
+                left: 0,
+              }}
+            >
+              <YAxis type="number" dataKey="local_score" hide />
+              <XAxis
+                dataKey="name"
+                type="category"
+                tickLine={false}
+                tickMargin={5}
+                axisLine={false}
+                tickFormatter={(value: string) =>
+                  value.length > 15 ? value.slice(0, 7) + "..." : value
+                }
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Bar dataKey="local_score" fill="var(--accent)" radius={5} />
+              <Bar dataKey="stars" fill="var(--chart-4)" radius={5} />
+            </BarChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
