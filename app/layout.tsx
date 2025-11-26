2e540fdd58ad82b0497139c68c5de64d43eed632
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2025-11-26 22:32:06 +0100

    chore: format code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,19 @@ import type { Metadata } from "next";
 import { Sora, Azeret_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import {ThemeProvider} from "@/src/components/theme-provider";
+import { ThemeProvider } from "@/src/components/theme-provider";
 
 const sora = Sora({
-    subsets: ["latin"],
-    weight: ["200", "400", "700"],
-    variable: "--font-sora"
-})
+  subsets: ["latin"],
+  weight: ["200", "400", "700"],
+  variable: "--font-sora",
+});
 
-const azeret = Azeret_Mono ({
-    subsets: ["latin"],
-    weight: ["200", "400", "600", "800"],
-    variable: "--font-azeret"
-})
+const azeret = Azeret_Mono({
+  subsets: ["latin"],
+  weight: ["200", "400", "600", "800"],
+  variable: "--font-azeret",
+});
 
 export const metadata: Metadata = {
   title: "Neuland Advent Of Code Leaderboard",
@@ -27,17 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body
-        className={`${sora.variable} ${azeret.variable} font-sora`}
-    >
-    <ThemeProvider
-        attribute={"class"}
-        defaultTheme={"dark"}
-        enableSystem={false}
-        disableTransitionOnChange
-    >
-        {children}
-      </ThemeProvider>
+      <body className={`${sora.variable} ${azeret.variable} font-sora`}>
+        <ThemeProvider
+          attribute={"class"}
+          defaultTheme={"dark"}
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
