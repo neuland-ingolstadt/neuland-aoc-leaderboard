7391e3ecc042163246ba6ddd8bb7af3b77c81c65
SparklingByte <118135751+SparklingByte@users.noreply.github.com>
2025-11-28 21:47:00 +0100

    feat: add footer

---
+++ components/layout/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div className="w-full border-t border-border py-4 mt-8">
+            <div className="container mx-auto px-4 text-center text-sm text-muted-foreground">
+                Neuland ist nicht für den Inhalt des Wettbewerbs zuständig sondern bietet
+                nur das Leaderboard mit Preisverleihung an
+            </div>
+        </div>
+    );
+}
